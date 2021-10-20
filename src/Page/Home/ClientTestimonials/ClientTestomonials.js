eacc0d3583990874f0596531c6c37f593f5fc15b
Fahad98723 <md.ravi63@gmail.com>
2021-10-20 14:10:23 +0600

    done

--- src/Page/Home/ClientTestimonials/ClientTestomonials.js
+++ src/Page/Home/ClientTestimonials/ClientTestomonials.js
@@ -23,7 +23,7 @@ const ClientTestomonials = () => {
                             </Col>
                         </Row> 
                         <Row>
-                            <Col lg = '6' className='mt-3 text-success fw-bold'>
+                            <Col lg = '12' className='mt-3 text-success fw-bold'>
                                 <p>On behalf of my wife, who was a patient in your hospital, I humbly submit my sincere gratitude to the management and staff of Jamaica Hospital, especially to your outstanding nurses and PCAs. They have been outstandingly helpful and provided a high quality of service, care and comfort to my wife. Thank you.</p>
                             </Col>    
                         </Row>  
@@ -39,7 +39,7 @@ const ClientTestomonials = () => {
                             </Col>
                         </Row> 
                         <Row>
-                            <Col lg = '6' className='mt-3 text-success fw-bold'>
+                            <Col lg = '12' className='mt-3 text-success fw-bold'>
                                 <p>I recently found myself under your care for a cardiac issue. While there, accompanied by my wife, we found that the entire staff at Jamaica Hospital was exceedingly professional and efficient, from your E.R. receptionists to the cardiac care team. You should be proud of your outstanding staff and service.</p>
                             </Col>    
                         </Row>  
